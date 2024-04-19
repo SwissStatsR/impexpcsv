34696fce07db3091bee8642bae848c3f820c9abb
gibo <gibo.gaf@gmail.com>
2024-04-19 19:48:21 +0200

    remove created objects

--- tests/testthat/test-import_list_csv.R
+++ tests/testthat/test-import_list_csv.R
@@ -5,8 +5,10 @@ test_that("import_list_csv works", {
   d1 <- fread(f[1])
   d2 <- fread(f[2])
   
-  d <- import_list_csv(f)
+  d <- import_list_csv(csv_files = f)
   
   expect_s3_class(d, "data.table")
-  expect_true((nrow(d1) + nrow(d2)) == nrow(d))
+  expect_equal((nrow(d1) + nrow(d2)), nrow(d))
+  
+  rm(p, d, f, d1, d2)
 })
