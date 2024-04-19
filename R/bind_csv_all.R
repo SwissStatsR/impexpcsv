e9fcfc4b7366b93cb78e2478cf910b81beee6dc9
gibo <gibo.gaf@gmail.com>
2024-04-19 18:49:15 +0200

    use import_existing_add_missing() inside bind_csv_all()

--- R/bind_csv_all.R
+++ R/bind_csv_all.R
@@ -20,22 +20,7 @@ bind_csv_all <- function(csv_files,
   stopifnot(is.character(col_names))
   
   # Import the first csv file, add missing columns and export to csv_output
-  col_names_first <- common_columns(csv_files[1], ...)
-  existing_cols <- base::intersect(
-    col_names, 
-    col_names_first
-  )
-  tmp <- data.table::fread(csv_files[1], select = existing_cols, ...)
-  
-  missing_cols <- base::setdiff(col_names, col_names_first)
-  
-  if (length(missing_cols) > 0) {
-    for (i in seq_along(missing_cols)) {
-      tmp[, (missing_cols[i]) := NA_character_]
-    }
-  }
-  
-  tmp <- tmp[, .SD, .SDcols = col_names]
+  tmp <- import_existing_add_missing(csv_files[1], col_names, ...)
   
   data.table::fwrite(tmp, file = csv_output, sep = sep_output, append = FALSE)
   
@@ -43,22 +28,7 @@ bind_csv_all <- function(csv_files,
   # csv_output
   if (length(csv_files) > 1L) {
     for (i in 2:length(csv_files)) {
-      col_names_first <- common_columns(csv_files[i], ...)
-      existing_cols <- base::intersect(
-        col_names, 
-        col_names_first
-      )
-      tmp <- data.table::fread(csv_files[i], select = existing_cols, ...)
-
-      missing_cols <- base::setdiff(col_names, col_names_first)
-      
-      if (length(missing_cols) > 0) {
-        for (j in seq_along(missing_cols)) {
-          tmp[, (missing_cols[j]) := NA_character_]
-        }
-      }
-      
-      tmp <- tmp[, .SD, .SDcols = col_names]
+      tmp <- import_existing_add_missing(csv_files[i], col_names, ...)
       
       data.table::fwrite(tmp, file = csv_output, sep = sep_output, append = TRUE)
     }
